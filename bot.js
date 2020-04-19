82a90216f1bd561edc16aa2b23e12a5400c6d65c
josemasf <josemasf@gmail.com>
2020-04-19 19:06:46 +0200

    cambio evento de mensajes para loggee

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+  console.log(msg);
 });
 
 bot.onText(/\/test (.+)/, (msg, match) => {
