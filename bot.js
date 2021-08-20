97fadd998270587d2b168746dd3201c56e5c9e6d
José María Santos Fernández <jm.santos@innovation.es>
2021-08-20 09:51:11 +0200

    console

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 const TeleBot = require('telebot');
 
 const token = process.env.FEDETOKEN;
-const bot = new TeleBot({
-  token,
-  usePlugins: ['regExpMessage', 'shortReply']
-});
+const bot = new TeleBot(token);
 
-bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
+bot.on(['/start', '/hello'], (msg) =>{
+  console.log(msg,'holaaaaa');
+  msg.reply.text('Welcome!')});
 
 bot.on('/cover', (msg) => {
+  console.log(msg, 'cover');
   const url = 'http://img.kiosko.net/2021/08/19/es/elpais.200.jpg';
   return msg.reply.photo(url);
 });
